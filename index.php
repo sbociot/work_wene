b8929d1fc3fe59b20942433d71adc41dd8425c05
Kevin Duvignau <kevin.duvignau40@gmail.com>
2017-06-21 14:45:34 -0400

    bug fixed

--- index.php
+++ index.php
@@ -1,205 +1,207 @@
 <?php require 'views/header.php'; ?>
-<div class="header_container">
-  <div class="mysideNav" >
-     <h1><span>SPINN</span><span>UP</span></h1>
-     <nav class="vertical_nav">
-       <ul class="menu">
-         <li><a href=""><i class="fa fa-file-text-o" style="font-size:24px"></i>Vue d'ensemble</a></li>
-         <li><a href=""><i class="fa fa-line-chart" style="font-size:24px"></i>Activités et stats</a></li>
-         <li><a href=""><i class="fa fa-bar-chart" style="font-size:24px"></i>Mes projets</a></li>
-         <li><a href=""><i class="fa fa-user-o" style="font-size:24px"></i>Profil d'artiste</a></li>
-         <li><a href=""><i class="fa fa-music" style="font-size:24px"></i>Les Pros</a></li>
-         <li><a href=""><i class="fa fa-file-o" style="font-size:24px"></i>Offres Partenariats</a></li>
-         <li><a href=""><i class="fa fa-newspaper-o" style="font-size:24px"></i>Actualités</a></li>
-         <li><a href="">+ Nouveau Projets</a></li>
-         <li><a href=""><i class="fa fa-cogs" style="font-size:24px"></i>Parametres</a></li>
-         <li><a href=""><i class="fa fa-user-plus" style="font-size:24px"></i>Deconnexion</a></li>
-       </ul>
-     </nav>
-   </div>
-  <div class="menu-toggle">
-     <div class="hamburger"></div>
-   </div>
-   <nav class="site-top-nav">
-     <ul class="top-menu">
-       <li><a href="#">Contacter l’assistance technique</a></li>
-       <li><a href="#">Aide / FAQ</a></li>
+<div class="mysideNav" >
+   <h1><span>SPINN</span><span>UP</span></h1>
+   <nav class="vertical_nav">
+     <ul class="menu">
+       <li><a href=""><i class="fa fa-file-text-o" style="font-size:24px"></i>Vue d'ensemble</a></li>
+       <li><a href=""><i class="fa fa-line-chart" style="font-size:24px"></i>Activités et stats</a></li>
+       <li><a href=""><i class="fa fa-bar-chart" style="font-size:24px"></i>Mes projets</a></li>
+       <li><a href=""><i class="fa fa-user-o" style="font-size:24px"></i>Profil d'artiste</a></li>
+       <li><a href=""><i class="fa fa-music" style="font-size:24px"></i>Les Pros</a></li>
+       <li><a href=""><i class="fa fa-file-o" style="font-size:24px"></i>Offres Partenariats</a></li>
+       <li><a href=""><i class="fa fa-newspaper-o" style="font-size:24px"></i>Actualités</a></li>
+       <li><a href="">+ Nouveau Projets</a></li>
+       <li><a href=""><i class="fa fa-cogs" style="font-size:24px"></i>Parametres</a></li>
+       <li><a href=""><i class="fa fa-user-plus" style="font-size:24px"></i>Deconnexion</a></li>
      </ul>
    </nav>
-   <div class="main_content">
-     <div class="content">
-       <ul class="content_wrapper">
-         <li>
-           <div class="project">
-             <span>Statut de vos projets</span>
-               <hr>
-                 <div class="project_status">
-                   <span></span>
-                   <span></span>
-                   <span></span>
-                 </div>
-                 <div class="top_bar_status">
-                   <span>2</span>
-                   <span>Projets incomplets</span>
-                   <span><a href="#">Visualiser tout</a></span>
-                 </div>
-                 <div class="status_content_wrapper">
-                   <div class="status_image">
-                     <img src="images/no-image-small(1).jpg" alt="Stat">
-                   </div>
-                   <div class="status_infos">
-                     <span> Untitle</span>
-                       <p>Ce projet n'a pas été complété. Cliquez sur Continuer pour le terminer et le distribuer sur les plateformes.</p>
-                       <a href="#">Continuer</a>
-                   </div>
-                 </div>
-                 <div class="projet_encour">
-                   <span>0</span>
-                   <span>Projets en cours de distribution</span>
-                   <span><a href="#">Visualiser Tout</a></span>
-                 </div>
-                 <hr>
-                 <div class="projet_disponible">
-                   <span>0</span>
-                   <span>Projets disponibles sur les plateformes digitales</span>
-                   <span><a href="#">Visualiser Tout</a></span>
-                 </div>
-                 <hr>
-                 <div class="vos_projets">
-                   <a href="#">Visualiser Mes projets</a>
-                 </div>
-             </div>
-         </li>
-         <li>
-           <div class="start_info">
-             <span>Commencer</span>
-             <p>Une fois votre musique distribuée sur les plateformes,
-               vous pouvez l'envoyer à un professionnel pour attirer son
-               attention. Lisez le profil de nos pros pour savoir à qui votre musique
-               est susceptible de correspondre le mieux. Ils écouteront votre musique et, en retour, vous laisseront parfois des commentaires ou enverront votre musique aux responsables artistiques de nos labels partenaires.</p>
-               <div class="start_link_info_plus">
-                 <a href="#">
-                     <span><i class="fa fa-check-square" style="font-size:24px;color:green"></i></span>
-                     <span>Ouvrer un compte spinnup</span>
-                     <span>Effectué</span>
-                   </span>
-                 </a>
-                 <a href="#">
-                   <span><i class="fa fa-chevron-circle-right" style="font-size:24px;color:red"></i></span>
-                   <span>Connectez vos réseau sociaux</span>
-                   <span>Non effecteé</span>
-                 </a>
-                 <a href="#">
-                   <span><i class="fa fa-chevron-circle-right" style="font-size:24px;color:red"></i></span>
-                   <span>Creer votre premier single,EP ou album</span>
-                   <span>Non effectué</span>
-                 </a>
-                 <a href="#">
-                   <span><i class="fa fa-chevron-circle-right" style="font-size:24px;color:red"></i></span>
-                   <span>Completer votre profil d'artiste</span>
-                   <span>Non effectué</span>
-                 </a>
-               </div>
-           </div>
-         </li>
-         <li>
-           <div class="section_pro">
-               <span>Nos pros sont impatients d'écouter votre musique</span>
-               <div class="pro_thumbnail">
-                 <img src="images/pros_images/362b07bd5b6cc1ae3ce58042f11b2735.jpeg" alt="">
-                 <img src="images/pros_images/1dc88f5da539ac694bbc2a46b8681230.jpeg" alt="">
-                 <img src="images/pros_images/182856c551dc34c1adabb62809f945ad.jpeg" alt="">
-                 <img src="images/pros_images/0ee85fb3f1ff90fa81bb80f1f451b0e3.jpeg" alt="">
-                 <img src="images/pros_images/e72865ded29a8f503bc34bf2f6489fd3.jpg" alt="">
-               </div>
-               <div class="pro_infos">
-                 <p>Une fois votre musique distribuée sur les plateformes,
-                    vous pouvez la mettre en avant auprès d'un professionnel
-                    pour attirer son attention. Lisez le profil de nos pros pour
-                    savoir à qui votre musique est susceptible de correspondre
-                     le mieux. Ils écouteront votre musique et, en retour,
-                      vous laisseront parfois des commentaires ou enverront
-                      votre musique aux responsables artistiques de nos labels
-                      partenaires.</p>
-               </div>
-               <div class="pro_infos_link">
-                 <a href="#">Voir les profils de nos pros</a>
-               </div>
-         </div>
-         </li>
-         <li>
-           <div class="spotify_chart">
-             <span>Surveillez les écoutes de vos morceaux sur Spotify au jour le jour</span>
-             <div class="link_for_statistique">
-                 <span class="example">Example de Graphique</span>
-               <div class="chart_canvas">
-                 <canvas id="myChart" height="350px" width="510"></canvas>
-               </div>
-               <a href="#">
-                 <span>Visualisez vos statistiques</span>
-               </a>
-             </div>
-           </div>
-         </li>
-         <li>
-           <div class="user_profils">
-             <span>Complétez votre profil d'artiste</span>
-             <div class="artist_thumbnails"><img src="images/profil_artiste.jpg" alt="">
-                 <p>Vous pouvez utiliser votre profil d'artiste pour vous auto-promouvoir et partager votre musique avec vos fans et nos pros.</p>
-             </div>
-             <div class="artist_infos_link">
-               <a href="#">Voir les profils de nos pros</a>
-             </div>
-           </div>
-         </li>
-       </ul>
-       <ul class="social_media_merge">
-         <li>
-           <div class="header_title_activity">
-             <span>Activité des pros</span>
-             <div class="activity_thumbnails">
-               <div class="music_activity">
-                 <div class="image_activity_container">
-                   <img src="images/listening.png" alt="Listening">
-                 </div>
-                 <div class="reference">
-                   <p>Si un pro consulte votre profil ou écoute votre musique, vous en serez informé(e) ici.</p>
-                 </div>
-             </div>
-               </div>
-               <div class="link_for_pro">
-                   <a href="#"><span>Visualiser l’activité des pros</span></a>
-               </div>
-           </div>
-
-         </li>
-         <li>
-           <div class="social_media">
-             <div class="social_media_info">
-               <span class="media_sociaux">Associer les comptes de vos réseaux sociaux</span>
-               <p>
-                 Associer les comptes de vos réseaux sociaux vous permet
-                  d’avoir une vue d’ensemble de vos fans en un seul et même
-                  endroit. Même si vous n’êtes pas encore prêt(e) à créer un single
-                   EP ou album, il est judicieux d’associer les comptes de vos différents
-                   réseaux sociaux pour attirer plus facilement l’attention de nos pros.</p>
-                   <div class="music_platform">
-                     <i class="fa fa-youtube" style="font-size:40px;color:white"></i>
-                     <i class="fa fa-soundcloud" style="font-size:40px;color:white"></i>
-                     <i class="fa fa-twitter" style="font-size:40px;color:white"></i>
-                     <i class="fa fa-facebook" style="font-size:40px;color:white"></i>
-                   </div>
-                   <div class="platform_social_link">
-                     <a href="#">
-                       <span>Connectez vos résaux sociaux</span>
-                     </a>
-                   </div></div>
-             </div>
-
-         </li>
-       </ul>
+ </div>
+<div class="main">
+  <div class="handler">
+    <div class="menu-toggle">
+       <div class="hamburger"></div>
      </div>
-   </div>
+     <nav class="site-top-nav">
+       <ul class="top-menu">
+         <li><a href="#">Contacter l’assistance technique</a></li>
+         <li><a href="#">Aide / FAQ</a></li>
+       </ul>
+     </nav>
   </div>
+  <div class="content">
+    <ul class="content_wrapper">
+      <li>
+        <div class="project">
+          <span>Statut de vos projets</span>
+            <hr>
+              <div class="project_status">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              <div class="top_bar_status">
+                <span>2</span>
+                <span>Projets incomplets</span>
+                <span><a href="#">Visualiser tout</a></span>
+              </div>
+              <div class="status_content_wrapper">
+                <div class="status_image">
+                  <img src="images/no-image-small(1).jpg" alt="Stat">
+                </div>
+                <div class="status_infos">
+                  <span> Untitle</span>
+                    <p>Ce projet n'a pas été complété. Cliquez sur Continuer pour le terminer et le distribuer sur les plateformes.</p>
+                    <a href="#">Continuer</a>
+                </div>
+              </div>
+              <div class="projet_encour">
+                <span>0</span>
+                <span>Projets en cours de distribution</span>
+                <span><a href="#">Visualiser Tout</a></span>
+              </div>
+              <hr>
+              <div class="projet_disponible">
+                <span>0</span>
+                <span>Projets disponibles sur les plateformes digitales</span>
+                <span><a href="#">Visualiser Tout</a></span>
+              </div>
+              <hr>
+              <div class="vos_projets">
+                <a href="#">Visualiser Mes projets</a>
+              </div>
+          </div>
+      </li>
+      <li>
+        <div class="start_info">
+          <span>Commencer</span>
+          <p>Une fois votre musique distribuée sur les plateformes,
+            vous pouvez l'envoyer à un professionnel pour attirer son
+            attention. Lisez le profil de nos pros pour savoir à qui votre musique
+            est susceptible de correspondre le mieux. Ils écouteront votre musique et, en retour, vous laisseront parfois des commentaires ou enverront votre musique aux responsables artistiques de nos labels partenaires.</p>
+            <div class="start_link_info_plus">
+              <a href="#">
+                  <span><i class="fa fa-check-square" style="font-size:24px;color:green"></i></span>
+                  <span>Ouvrer un compte spinnup</span>
+                  <span>Effectué</span>
+                </span>
+              </a>
+              <a href="#">
+                <span><i class="fa fa-chevron-circle-right" style="font-size:24px;color:red"></i></span>
+                <span>Connectez vos réseau sociaux</span>
+                <span>Non effecteé</span>
+              </a>
+              <a href="#">
+                <span><i class="fa fa-chevron-circle-right" style="font-size:24px;color:red"></i></span>
+                <span>Creer votre premier single,EP ou album</span>
+                <span>Non effectué</span>
+              </a>
+              <a href="#">
+                <span><i class="fa fa-chevron-circle-right" style="font-size:24px;color:red"></i></span>
+                <span>Completer votre profil d'artiste</span>
+                <span>Non effectué</span>
+              </a>
+            </div>
+        </div>
+      </li>
+      <li>
+        <div class="section_pro">
+            <span>Nos pros sont impatients d'écouter votre musique</span>
+            <div class="pro_thumbnail">
+              <img src="images/pros_images/362b07bd5b6cc1ae3ce58042f11b2735.jpeg" alt="">
+              <img src="images/pros_images/1dc88f5da539ac694bbc2a46b8681230.jpeg" alt="">
+              <img src="images/pros_images/182856c551dc34c1adabb62809f945ad.jpeg" alt="">
+              <img src="images/pros_images/0ee85fb3f1ff90fa81bb80f1f451b0e3.jpeg" alt="">
+              <img src="images/pros_images/e72865ded29a8f503bc34bf2f6489fd3.jpg" alt="">
+            </div>
+            <div class="pro_infos">
+              <p>Une fois votre musique distribuée sur les plateformes,
+                 vous pouvez la mettre en avant auprès d'un professionnel
+                 pour attirer son attention. Lisez le profil de nos pros pour
+                 savoir à qui votre musique est susceptible de correspondre
+                  le mieux. Ils écouteront votre musique et, en retour,
+                   vous laisseront parfois des commentaires ou enverront
+                   votre musique aux responsables artistiques de nos labels
+                   partenaires.</p>
+            </div>
+            <div class="pro_infos_link">
+              <a href="#">Voir les profils de nos pros</a>
+            </div>
+      </div>
+      </li>
+      <li>
+        <div class="spotify_chart">
+          <span>Surveillez les écoutes de vos morceaux sur Spotify au jour le jour</span>
+          <div class="link_for_statistique">
+              <span class="example">Example de Graphique</span>
+            <div class="chart_canvas">
+              <canvas id="myChart" height="350px" width="510"></canvas>
+            </div>
+            <a href="#">
+              <span>Visualisez vos statistiques</span>
+            </a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="user_profils">
+          <span>Complétez votre profil d'artiste</span>
+          <div class="artist_thumbnails"><img src="images/profil_artiste.jpg" alt="">
+              <p>Vous pouvez utiliser votre profil d'artiste pour vous auto-promouvoir et partager votre musique avec vos fans et nos pros.</p>
+          </div>
+          <div class="artist_infos_link">
+            <a href="#">Voir les profils de nos pros</a>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <ul class="social_media_merge">
+      <li>
+        <div class="header_title_activity">
+          <span>Activité des pros</span>
+          <div class="activity_thumbnails">
+            <div class="music_activity">
+              <div class="image_activity_container">
+                <img src="images/listening.png" alt="Listening">
+              </div>
+              <div class="reference">
+                <p>Si un pro consulte votre profil ou écoute votre musique, vous en serez informé(e) ici.</p>
+              </div>
+          </div>
+            </div>
+            <div class="link_for_pro">
+                <a href="#"><span>Visualiser l’activité des pros</span></a>
+            </div>
+        </div>
+
+      </li>
+      <li>
+        <div class="social_media">
+          <div class="social_media_info">
+            <span class="media_sociaux">Associer les comptes de vos réseaux sociaux</span>
+            <p>
+              Associer les comptes de vos réseaux sociaux vous permet
+               d’avoir une vue d’ensemble de vos fans en un seul et même
+               endroit. Même si vous n’êtes pas encore prêt(e) à créer un single
+                EP ou album, il est judicieux d’associer les comptes de vos différents
+                réseaux sociaux pour attirer plus facilement l’attention de nos pros.</p>
+                <div class="music_platform">
+                  <i class="fa fa-youtube" style="font-size:40px;color:white"></i>
+                  <i class="fa fa-soundcloud" style="font-size:40px;color:white"></i>
+                  <i class="fa fa-twitter" style="font-size:40px;color:white"></i>
+                  <i class="fa fa-facebook" style="font-size:40px;color:white"></i>
+                </div>
+                <div class="platform_social_link">
+                  <a href="#">
+                    <span>Connectez vos résaux sociaux</span>
+                  </a>
+                </div></div>
+          </div>
+
+      </li>
+    </ul>
+  </div>
+</div>
+ </div>
+
 
 <?php require 'views/footer.php'; ?>
--- script/master.js
+++ script/master.js
@@ -1,14 +1,20 @@
 (function() {
   "use strict";
-  $('.menu-toggle').on("click",function() {
-    $(".header_container").toggleClass('open_sideNav');
-    $(".mysideNav").toggleClass('side_nav_open');
-    if($('div').eq(0).hasClass('side_nav_open')){
-      console.log('yes');
-      $(".main_content").toggleClass('push_content');
-    }else{
-        $(".main_content").toggleClass('push_content');
-    }
-  });
+  // $('.menu-toggle').on("click",function() {
+  //   $(".header_container").toggleClass('open_sideNav');
+  //   $(".mysideNav").toggleClass('side_nav_open');
+  //   if($('div').eq(0).hasClass('side_nav_open')){
+  //     console.log('yes');
+  //     $(".main_content").toggleClass('push_content');
+  //   }else{
+  //       $(".main_content").toggleClass('push_content');
+  //   }
+  // });
+$(".menu-toggle").on("click",function(){
+  console.log("ok");
+  $(".mysideNav").toggleClass('open_sideNav');
+  $(".main").toggleClass('slide_content');
+  $(".menu-toggle").toggleClass('hamburger_open');
 
+});
 })();
